9a453de07cd4427e25c4e666c8b3e3e303b066f2
rustinpc <rustinpc@gmail.com>
2014-11-21 19:37:56 -0800

    Established connection with database.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -5,7 +5,9 @@ var bluebird = require('bluebird');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
+    get: function (req, res) {
+      models.messages.get();
+    }, // a function which handles a get request for all messages
     post: function (req, res) {} // a function which handles posting a message to the database
   },
 
--- server/models/index.js
+++ server/models/index.js
@@ -5,7 +5,18 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
+    get: function () {
+      db.connection.connect();
+      console.log('connected');
+      db.connection.query('SELECT * FROM messages', function(err, rows){
+        if(err) {
+          console.log('query error')
+          throw err;
+        } else {
+          console.log( "data",rows );
+        }
+      });
+    }, // a function which produces all the messages
     post: function () {} // a function which can be used to insert a message into the database
   },
 
