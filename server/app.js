c41ce8ac4802057305e49c3b1868dfd5d2e385e9
rustinpc <rustinpc@gmail.com>
2014-11-22 10:29:13 -0800

    connected client with server using express

--- server/app.js
+++ server/app.js
@@ -19,13 +19,13 @@ app.set("port", 3000);
 app.use(morgan('dev'));
 app.use(parser.json());
 
-app.use('/', router);
+// app.use('/', router);
 
 // Set up our routes
 app.use("/classes", router);
 
 // Serve the client files
-app.use(express.static(__dirname + "../client"));
+app.use(express.static(__dirname + "/../client"));
 
 // If we are being run directly, run the server.
 if (!module.parent) {
